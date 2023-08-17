62528876b00c47207c3dbaa5cc9984e04512d410
Xander Forge <xand3r40r93@gmail.com>
2023-08-17 16:58:27 +0100

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
-wget https://github.com/xand3r40r93/prtouch_v2_fan/raw/master/prtouch_v2_fan.pyc -O /usr/share/klipper/klippy/extras/prtouch_v2_fan.pyc
-if [ -z `cat /usr/data/printer_data/config/printer.cfg | grep prtouch_v2_fan` ]; then
-  echo [prtouch_v2_fan] >> /usr/data/printer_data/config/printer.cfg
+mkdir -p /usr/share/klipper/klippy/extras/prtouch_v2_fan
+wget https://github.com/xand3r40r93/prtouch_v2_fan/raw/master/__init__.py -O /usr/share/klipper/klippy/extras/prtouch_v2_fan/__init__.py
+wget https://github.com/xand3r40r93/prtouch_v2_fan/raw/master/prtouch_v2_fan.pyc -O /usr/share/klipper/klippy/extras/prtouch_v2_fan/prtouch_v2_fan.pyc
+if [ -z `cat /usr/data/printer_data/config/printer_params.cfg | grep prtouch_v2_fan` ]; then
+  echo [prtouch_v2_fan] >> /usr/data/printer_data/config/printer_params.cfg
 fi
 /etc/init.d/S55klipper_service restart
